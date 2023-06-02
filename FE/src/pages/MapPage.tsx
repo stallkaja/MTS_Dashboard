5666dbde76f840250abb2cb2161243ecaba70dcf
Leath <Jonathan.Leath@lamresearch.com>
2023-06-02 16:55:52 -0700

    Moved 2 more images to image folder. Also deleted a duplicate image. Changed any reference paths to old image locations to their new locations.

--- FE/src/pages/MapPage.tsx
+++ FE/src/pages/MapPage.tsx
@@ -1,6 +1,6 @@
 
 import React, { Component }  from 'react';
-import dosmap from '../images/DOSMap.png'; //Imports DOS Map from the image folder, yet gives syntax error.
+import dosmap from '../images/DOSMap.png'; /* Imports DOS Map from the image folder, yet gives syntax error. If anyone knows why, let me know. -LeathJo 06/02/23 */
 import ReactSearchBox from "react-search-box";
   
 const MapPage = () => {
@@ -14,11 +14,11 @@ const MapPage = () => {
     >
       <h1>Map Page</h1>
       <ReactSearchBox
-      placeholder="Search for John, Jane or Mary"
+      placeholder="Search for Jon, Jane or Mary"
       data={[
         {
-          key: "john",
-          value: "John Doe"
+          key: "jon",
+          value: "Jon Doe"
         },
         {
           key: "jane",
@@ -45,10 +45,11 @@ const MapPage = () => {
       autoFocus
       leftIcon={<>🔎</>}
       iconBoxSize="28px"
-    />|
+    />
+        {/* This is displaying the map, currently I cannot get it to center underneath the header, but its a WIP, feel free to assist if bored. -LeathJo 06/02/23 */
         <img 
-          src={dosmap} alt="map" width="65%" height="65%">
-        </img>
+          src={dosmap} alt="map" width="80%" height="80%">
+        </img>}
     </div>
   );
 };
