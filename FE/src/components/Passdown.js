74f0b1c79aa182774f027c192619c49c1819d820
JustinHumble <120693705+JustinHumble@users.noreply.github.com>
2023-07-15 16:43:21 -0700

    adding passdown css

---
+++ FE/src/components/Passdown.js
@@ -0,0 +1,5 @@
+import { useState, useEffect, useRef } from 'react';
+import { Button, Input, Space, Table, typography } from 'antd';
+import { useNavigate, Link } from 'react-router';
+import { SearchOutlined } from '@ant-design/icons';
+
