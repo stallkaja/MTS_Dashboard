94dc865690f155840d96983ac4adf50aa5dab934
Oster <Jordan.Oster@lamresearch.com>
2024-01-11 21:06:14 -0800

    added tables to pull list page

--- FE/src/pages/toolPullListPage.js
+++ FE/src/pages/toolPullListPage.js
@@ -6,10 +6,14 @@ import './toolPullList.css';
 import { Button, ConfigProvider, DatePicker } from 'antd';
 import dayjs from 'dayjs';
 import { useState, useEffect } from 'react';
+import ColumnChange from '../components/ColumnChange';
+import StaggerPullListTable from '../components/StaggerPullListTable';
+import OverdueListTable from '../components/OverdueListTable';
+import LostListTable from '../components/LostListTable';
 
 
   
-const ToolHistoryPage = () => {
+const ToolPullListPage = () => {
     const navigate = useNavigate();
     const GenList = () => {
         alert('James!!!!')
@@ -17,6 +21,22 @@ const ToolHistoryPage = () => {
     const [today, setToday] = useState(dayjs());
     const handleDate = (date, dateString) => {
         setToday(dateString)
+    }
+    const [hiddenArray, setHiddenArray] = useState({
+        key: 1,
+        keey: 2,
+        keeey: 3
+    });
+    const [hideList, setHideList] = useState([]);
+    const OpenTicket = () => {
+        let path = '/TicketPage';
+        navigate(path);
+    }
+    //Data being returned from columnChange component, to be passed to child tables
+    const parent = (childData) => {
+        return (
+            setHiddenArray(childData)
+        )
     }
 
     return (
@@ -33,6 +53,12 @@ const ToolHistoryPage = () => {
             <div>
                 <div id='PullTitleCard'>
                     <h1 id='ToolTitle'>Staggering List</h1>
+                    <h1 id='ToolSearch'>
+                        <ColumnChange
+                            tName='caltoolstable'
+                            parentPass={parent}
+                            hideList={hideList} />
+                    </h1>
                 </div>
 
                 <div id='ToolHeader'>
@@ -59,17 +85,22 @@ const ToolHistoryPage = () => {
                         </Button>
                     </div>
                 </div>
+                <StaggerPullListTable
+                    hideArray={hiddenArray} />
                 <div id='OverdueCard'>
                     <div id='OverdueText'>Overdue Tools</div>
                 </div>
+                <OverdueListTable
+                    hideArray={hiddenArray} />
                 <div id='LostCard'>
                     <div id='LostText'>Lost Tools</div>
                 </div>
-                    
+                <LostListTable
+                    hideArray={hiddenArray} />    
                     
             </div>
         </ConfigProvider>
     );
 };
   
-export default ToolHistoryPage;
+export default ToolPullListPage;
